601a76eec0b0e0e782fb73ef7f0465c1778ecca4
aventusm <8antonm@gmail.com>
2022-01-13 17:51:32 +0200

    Upgrade to Expo 44. Emulator camera was not supported in the first place, might not get fixes there (works well on a real device though). v44 moves react-native-reanimated library dependency up which breaks the placeholder skeleton library. Consider making own placeholders

--- client/src/components/Placeholders/MainScreenPlaceholder.jsx
+++ client/src/components/Placeholders/MainScreenPlaceholder.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { Dimensions } from "react-native";
-import SkeletonContent from 'react-native-skeleton-content';
+import { Dimensions, Text } from "react-native";
+//import SkeletonContent from 'react-native-skeleton-content';
 
+// https://github.com/alexZajac/react-native-skeleton-content/issues/59
+// Expo 44 pushed the react-native-reanimated to 2.3.1 whereas the library above requires 2.2.0
+// Consider making own loader componen
 export const MainScreenPlaceholder = () => {
   return (
-    <SkeletonContent
+    <Text>Skeleton library placeholder</Text>
+/*     <SkeletonContent
       containerStyle={{flex: 1, width: "100%", justifyContent: "center", alignItems: "flex-start", marginLeft: 20, marginTop: 20}}
       animationDirection="horizontalLeft"
       duration={1500}
@@ -17,5 +21,6 @@ export const MainScreenPlaceholder = () => {
       ]}
       isLoading={true}>
     </SkeletonContent>
+*/
   );
 };
